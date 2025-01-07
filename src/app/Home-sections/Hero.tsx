c688d55b6bc056fabc29face8362246a197741e6
Abdelrahman_Magdy <abdalrahman.magdy.elawady@gmail.com>
2025-01-07 06:01:29 +0200

    add css equation to text in hero section

--- src/app/Home-sections/Hero.tsx
+++ src/app/Home-sections/Hero.tsx
@@ -6,12 +6,12 @@ export default function Hero() {
   return (
     <SectionContainer className="flex flex-col gap-8 lg:flex-row md:justify-between items-center 2xl:py-32">
       <div className="w-full flex flex-col gap-8 items-start lg:w-[572px]">
-        <h1 className="font-semibold text-[31px]  lg:text-[35px] 2xl:text-[49px]">
+        <h1 className="font-semibold text-[calc(31px+(49-31)*((clamp(390px,100vw,1512px)-390px)/(1512-390)))]">
           <span className="text-secondary-orange">Nitros</span> platform gets
           you shipping carriers at the
           <span className="text-secondary-orange"> best prices</span>
         </h1>
-        <p className="2xl:text-xl">
+        <p className="text-[calc(16px+(20-16)*((clamp(390px,100vw,1512px)-390px)/(1512-390)))]">
           Link your e-store with nitros with few steps to ship from you door to
           client&apos;s door
         </p>
